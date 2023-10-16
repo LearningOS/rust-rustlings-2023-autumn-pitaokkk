156681dd733f27e864d70ff98720dd4195c49441
pitaokkk <96687564+pitaokkk@users.noreply.github.com>
2023-10-16 03:34:49 +0000

    update

--- exercises/intro/intro2.rs
+++ exercises/intro/intro2.rs
@@ -5,8 +5,10 @@
 // Execute `rustlings hint intro2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 fn main() {
-    println!("Hello {}!");
+
+const S: &str = "word";
+    println!("Hello {S}!,");
 }
